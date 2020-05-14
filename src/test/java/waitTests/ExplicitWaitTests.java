26efd509433bc659444eaf0941477c6615ffbf28
Krzysztof <k.tabor@wp.pl>
2020-05-14 21:12:48 +0200

    Drugie repozytorium

--- src/test/java/waitTests/ExplicitWaitTests.java
+++ src/test/java/waitTests/ExplicitWaitTests.java
@@ -1,30 +1,19 @@
 package waitTests;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.*;
 
-public class ExplicitWaitTests {
-
-    private WebDriver driver;
-
-    @BeforeMethod
-    public void beforeTest() {
-        System.setProperty("webdriver.chrome.driver", "C:/drivers/chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.navigate().to("http://theinternet.przyklady.javastart.pl/dynamic_controls");
-    }
+public class ExplicitWaitTests extends BeforeAfterM {
 
     @Test
     public void waitForDisappearingElement() {
+
+        driver.navigate().to("http://theinternet.przyklady.javastart.pl/dynamic_controls");
 
         // znajdz checkbox
         WebElement checkbox = driver.findElement(By.id("checkbox"));
@@ -45,10 +34,6 @@ public class ExplicitWaitTests {
         WebElement messageLabel = driver.findElement(By.id("message"));
         assertEquals(messageLabel.getText(), "It's gone!");
     }
-
-    @AfterMethod
-    public void afterTest() {
-        driver.close();
-        driver.quit();
-    }
 }
+
+
